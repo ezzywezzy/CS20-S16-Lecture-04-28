4897771719de5217ca9d3f86737c5521063b8e71
Ezra Kosviner <ezrakosviner@umail.ucsb.edu>
2016-04-28 11:32:25 -0700

    initial circle function files

---
+++ circle_functions.py
@@ -0,0 +1,6 @@
+def areaCircle(radius):
+    '''compute area of the circle with defined radius'''
+    return "stubbywubby"
+def circumferenceCircle(radius):
+    '''compute circumference of the circle with defined radius'''
+    return "stubbywubby"
---
+++ test_circle.py
@@ -0,0 +1,12 @@
+import pytest
+def test_areaCircle_1():
+    assert(areaCircle(1.0)== 3.1415926535)
+def test_circumfernceCircle_1():
+    assert(circumferenceCircle(1.0)== 6.283185307179586)
+def test_areaCircle_1():
+    assert(areaCircle(3.0)== 28.274333882308138)
+def test_circumfernceCircle_1():
+    assert(circumferenceCircle(1.0)== 18.84955592153876)
+
+if __name__ == "__manin__":
+    pytest.main()
